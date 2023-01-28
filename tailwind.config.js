e2e97a8de41752e2840294b7764cb4174d25caa6
chrisriexp <105810816+chrisriexp@users.noreply.github.com>
2023-01-28 14:29:02 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./resources/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif']
+      },
+      colors: {
+      'custom-blue': '#5080C7',
+      'custom-gray': '#4c4c4c',
+      'custom-light-blue': '#EFF3F6'
+      },
+      boxShadow: {
+      'newdrop': '0 4px 50px #00000040;',
+      },
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+}
